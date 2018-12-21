8d65782a354be44c878bab59fb7e031621b864ac
Alexander Netter <xan@AlexandersMBP2.lan1>
2018-12-21 01:15:40 -0500

    Updates

---
+++ precache-manifest.c1473b19213f7e2aedfdbbfe72520180.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9b780ca6b294f882348",
+    "url": "/myapp/static/css/main.887d483e.chunk.css"
+  },
+  {
+    "revision": "e9b780ca6b294f882348",
+    "url": "/myapp/static/js/main.e9b780ca.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/myapp/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "86da97f6a9be0b2021a8caa57ca6509d",
+    "url": "/myapp/static/media/angelfish.86da97f6.jpg"
+  },
+  {
+    "revision": "30d339a0dc29272d2b56754318ae7de3",
+    "url": "/myapp/static/media/barracuda.30d339a0.jpeg"
+  },
+  {
+    "revision": "494a2de35f4666ee575bcb9089010e01",
+    "url": "/myapp/static/media/butterfly.494a2de3.jpg"
+  },
+  {
+    "revision": "4bd1e5d37a47ac27cab2b940a64ede9e",
+    "url": "/myapp/static/media/chromis.4bd1e5d3.jpg"
+  },
+  {
+    "revision": "77286bc8f39c2274e276389a4cb22629",
+    "url": "/myapp/static/media/clownfish.77286bc8.jpg"
+  },
+  {
+    "revision": "28673661af72bc26294f313761b34017",
+    "url": "/myapp/static/media/damselfish.28673661.jpg"
+  },
+  {
+    "revision": "f68e65709c231bf293880be90a42b3ed",
+    "url": "/myapp/static/media/grunt.f68e6570.jpg"
+  },
+  {
+    "revision": "958a46ee14e73148c19d40984d92681a",
+    "url": "/myapp/static/media/parrotfish.958a46ee.jpg"
+  },
+  {
+    "revision": "167eb737574d5c9efa8919279f7545cd",
+    "url": "/myapp/static/media/rockbeauty.167eb737.jpg"
+  },
+  {
+    "revision": "0af60fe26af9248a6561e6675534db40",
+    "url": "/myapp/static/media/sergeantmajor.0af60fe2.JPG"
+  },
+  {
+    "revision": "522c3fe584f4943f580255baf00d679b",
+    "url": "/myapp/static/media/tang.522c3fe5.jpg"
+  },
+  {
+    "revision": "c45bc1d1d55d1a03183e6f251bdc897a",
+    "url": "/myapp/index.html"
+  }
+];
