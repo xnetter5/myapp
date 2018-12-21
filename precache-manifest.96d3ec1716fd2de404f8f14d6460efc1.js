808ce56dda410a298d316ff261d217b79fd2096c
Alexander Netter <xan@AlexandersMBP2.lan1>
2018-12-21 01:12:30 -0500

    Updates

---
+++ precache-manifest.96d3ec1716fd2de404f8f14d6460efc1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9b780ca6b294f882348",
+    "url": "/my-app/static/css/main.887d483e.chunk.css"
+  },
+  {
+    "revision": "e9b780ca6b294f882348",
+    "url": "/my-app/static/js/main.e9b780ca.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/my-app/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "86da97f6a9be0b2021a8caa57ca6509d",
+    "url": "/my-app/static/media/angelfish.86da97f6.jpg"
+  },
+  {
+    "revision": "30d339a0dc29272d2b56754318ae7de3",
+    "url": "/my-app/static/media/barracuda.30d339a0.jpeg"
+  },
+  {
+    "revision": "494a2de35f4666ee575bcb9089010e01",
+    "url": "/my-app/static/media/butterfly.494a2de3.jpg"
+  },
+  {
+    "revision": "4bd1e5d37a47ac27cab2b940a64ede9e",
+    "url": "/my-app/static/media/chromis.4bd1e5d3.jpg"
+  },
+  {
+    "revision": "77286bc8f39c2274e276389a4cb22629",
+    "url": "/my-app/static/media/clownfish.77286bc8.jpg"
+  },
+  {
+    "revision": "28673661af72bc26294f313761b34017",
+    "url": "/my-app/static/media/damselfish.28673661.jpg"
+  },
+  {
+    "revision": "f68e65709c231bf293880be90a42b3ed",
+    "url": "/my-app/static/media/grunt.f68e6570.jpg"
+  },
+  {
+    "revision": "958a46ee14e73148c19d40984d92681a",
+    "url": "/my-app/static/media/parrotfish.958a46ee.jpg"
+  },
+  {
+    "revision": "167eb737574d5c9efa8919279f7545cd",
+    "url": "/my-app/static/media/rockbeauty.167eb737.jpg"
+  },
+  {
+    "revision": "0af60fe26af9248a6561e6675534db40",
+    "url": "/my-app/static/media/sergeantmajor.0af60fe2.JPG"
+  },
+  {
+    "revision": "522c3fe584f4943f580255baf00d679b",
+    "url": "/my-app/static/media/tang.522c3fe5.jpg"
+  },
+  {
+    "revision": "e5c4ec415546e80f48525b3419f2f64f",
+    "url": "/my-app/index.html"
+  }
+];
